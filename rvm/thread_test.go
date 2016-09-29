d566e6ad917f81fa7c0ae7281871bddf7785351e
Noel Cower <ncower@gmail.com>
2016-09-28 21:55:59 -0700

    Log instruction in test
    Change-Id: Iac2bf913083e1e45a88d49d4200dbc085db8de63

--- rvm/thread_test.go
+++ rvm/thread_test.go
@@ -12,6 +12,8 @@ func TestOpAdd(t *testing.T) {
 	th.Push(5)
 
 	instr := mkBinaryInstr(OpAdd, RegisterIndex(2), StackIndex(-1), constIndex(0))
+	t.Log(instr)
+
 	instr.execer()(instr, th)
 	if got := th.At(RegisterIndex(2)); got != vnum(15.3) {
 		t.Fatalf("th.At(RegisterIndex(2)) = %q; want %q", got, vnum(15.3))
